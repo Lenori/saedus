be0f604d14c7a4ff263a46cc4f2a067907052be8
Fábio Lourenço <faboyds@gmail.com>
2020-04-23 20:23:38 +0100

    Added new logo

--- proxy.config.js
+++ proxy.config.js
@@ -2,6 +2,7 @@ const proxy = [
   {
     context: '/api',
     target: 'http://18.224.180.162/api',
+    //target: 'http://127.0.0.1:8888',
     "secure": false,
     "changeOrigin": true,
     "logLevel": "debug",
